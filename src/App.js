16c02d84eaabf085f1624137acfb7800cb15cc54
KARTHIKEYAN V V <111226561+Karthikeyanveerarasu@users.noreply.github.com>
2023-07-15 13:42:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Testimonials from './Testimonial.js'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Testimonials</h1>
+        <Testimonials />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Testimonial.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import './styles.css'
+import alex from './images/alex.jpg'
+export default class Testimonials extends Component {
+  render() {
+    return (
+      <Carousel
+        showArrows={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={6100}
+      >
+        <div>
+          <img src={alex} />
+          <div className="myCarousel">
+            <h3>Shirley Fultz</h3>
+            <h4>Designer</h4>
+            <p>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <img src="/images/daniel.png" />
+          <div className="myCarousel">
+            <h3>Daniel Keystone</h3>
+            <h4>Designer</h4>
+            <p>
+              The simple and intuitive design makes it easy for me use. I highly
+              recommend Fetch to my peers.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <img src="/images/theo.png" />
+          <div className="myCarousel">
+            <h3>Theo Sorel</h3>
+            <h4>Designer</h4>
+            <p>
+              I enjoy catching up with Fetch on my laptop, or on my phone when
+              I'm on the go!
+            </p>
+          </div>
+        </div>
+      </Carousel>
+    );
+  }
+}
